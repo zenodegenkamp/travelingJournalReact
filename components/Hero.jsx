3c010814a34fe86d649f4f4dc7db60f712606317
zenode <46578099+zenodegenkamp@users.noreply.github.com>
2023-07-07 18:33:28 +0200

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -18,18 +18,3 @@ export default function Hero(){
     )
 }
 
-
-// export default function Hero(){
-//     return (
-//         <div className="hero">
-//             <div className="hero-container">
-//                 <h1 className="title-text">My adventure in Thailand</h1>
-//                 <h3 className="small-text"><bold>My top 5 locations</bold> during 2-month trip</h3>
-//                 <div className="profile-container">
-//                     <img className ="profile-pic" src="../images/profile-picture.jpeg" />
-//                     <p className="small-text">Zeno Degenkamp</p>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
